abec58c34231ca553ff86722a2fd4a22b4f2193a
twinbird827 <twinbird827@gmail.com>
2019-05-13 10:15:57 +0900

    [add] ｸﾗｽ名ﾌﾙﾊﾟｽの取得ﾃｽﾄ

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Parent();
+            var c = new Child();
+
+            Console.WriteLine(p.GetType().FullName);
+            Console.WriteLine(c.GetType().FullName);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Parent
+    {
+
+    }
+
+    class Child : Parent
+    {
+
+    }
+}
